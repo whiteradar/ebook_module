dee0d65ca3743b0c4725ce23ef0b11bce8a7bf3c
whiteradar <muhammadfaisal7@hotmail.com>
2023-10-24 21:40:14 +0800

    Update MainController.java
    Test CI/CD

--- src/main/java/com/example/ebookmodule/MainController.java
+++ src/main/java/com/example/ebookmodule/MainController.java
@@ -31,7 +31,7 @@ public class MainController {
         return "Saved";
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path="/everything")
     public @ResponseBody Iterable<EBook> getAllEBooks() {
         // This returns a JSON or XML with the users
         return eBookRepository.findAll();
