85c8ad99e0f3a306b246c99f62a0b7e83bf9726d
whiteradar <muhammadfaisal7@hotmail.com>
2023-10-29 21:37:54 +0800

    Testing automated testing

--- src/test/java/com/example/ebookmodule/EBookModuleApplicationTests.java
+++ src/test/java/com/example/ebookmodule/EBookModuleApplicationTests.java
@@ -14,42 +14,42 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes = EBookModuleApplication.class)
 public class EBookModuleApplicationTests {
+
+	@Autowired
+	private MainController mainController;
+
+	@MockBean
+	private EBookRepository eBookRepository;
+
+
+	void contextLoads() {
+		// Add your test logic here
+	}
+	@Test
+	public void testFindEbookById() {
+		// Set up mock behavior for eBookRepository
+//		when(eBookRepository.findById(1)).thenReturn(Optional.of(new EBook(1, "The Great Gatsby", "F. Scott Fitzgerald", 12.99)));
+
+		Optional<EBook> optionalEBook = eBookRepository.findById(1);
+		if (optionalEBook.isPresent()) {
+//			EBook ebook = optionalEBook.get();
+			// You can use 'ebook' within this block.
+			// Assertions to check the result
+			assertNotNull(optionalEBook);
+//			assertEquals("The Great Gatsby", optionalEBook.getTitle());
+		} else {
+			// Handle the case when the EBook is not found (optionalEBook is empty).
+		}
+
+		// Call the Controller method
+//		EBook ebook = mainController.getOneBook(1);
 //
-//	@Autowired
-//	private MainController mainController;
-//
-//	@MockBean
-//	private EBookRepository eBookRepository;
-//
-////	@Test
-////	void contextLoads() {
-////		// Add your test logic here
-////	}
-//
-////	public void testFindEbookById() {
-////		// Set up mock behavior for eBookRepository
-//////		when(eBookRepository.findById(1)).thenReturn(Optional.of(new EBook(1, "The Great Gatsby", "F. Scott Fitzgerald", 12.99)));
-////
-////		Optional<EBook> optionalEBook = eBookRepository.findById(1);
-////		if (optionalEBook.isPresent()) {
-//////			EBook ebook = optionalEBook.get();
-////			// You can use 'ebook' within this block.
-////			// Assertions to check the result
-////			assertNotNull(optionalEBook);
-////			assertEquals("The Great Gatsby", optionalEBook.getTitle());
-////		} else {
-////			// Handle the case when the EBook is not found (optionalEBook is empty).
-////		}
-////
-////		// Call the Controller method
-////		EBook ebook = mainController.getOneBook(1);
-////
-////		// Assertions to check the result
-////		assertNotNull(ebook);
-////		assertEquals("The Great Gatsby", ebook.getTitle());
-////	}
+//		// Assertions to check the result
+//		assertNotNull(ebook);
+//		assertEquals("The Great Gatsby", ebook.getTitle());
+	}
 //	@Test
 //	public void testGetAllEBooks() {
 //		// Set up mock behavior for eBookRepository
