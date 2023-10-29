e723832c523095b15ab10e9743366107b8653a4e
whiteradar <muhammadfaisal7@hotmail.com>
2023-10-29 18:30:02 +0800

    Uncomment mapping

--- src/main/java/com/example/ebookmodule/MainController.java
+++ src/main/java/com/example/ebookmodule/MainController.java
@@ -3,11 +3,11 @@ package com.example.ebookmodule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
 
